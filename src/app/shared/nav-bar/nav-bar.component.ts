6fefdcee747afebe4fbada8f5fc345687d82e32e
OSC-ENT-MOSADDIK <mosaddik.hafiz@oscillosoft.com.au>
2020-03-16 15:59:08 +0600

    nav-bar

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
-
+export class NavBarComponent implements OnInit,AfterViewInit {
+  @ViewChild('nav') nav;
   constructor() { }
-
+  ngAfterViewInit(): void {
+   
+  }
+  
   ngOnInit() {
+  }
+
+  click(){
+    //console.log("Hello world");
+    console.log(this.nav.nativeElement);
   }
 
 }
